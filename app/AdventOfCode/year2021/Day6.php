b55706db784a13784655f842c3be5242f6714c4c
Wesley Lether <wesleylether@gmail.com>
2021-12-06 14:39:39 +0100

    Added Year 2021 Day 6 part 2

--- app/AdventOfCode/year2021/Day6.php
+++ app/AdventOfCode/year2021/Day6.php
@@ -4,17 +4,46 @@ declare(strict_types=1);
 namespace App\AdventOfCode\year2021;
 
 use App\AdventOfCode\BaseAdventOfCodeDay;
+use function array_key_exists;
 use function array_map;
-use function dd;
+use function array_reduce;
+use function array_sum;
 use function explode;
+use function ksort;
 
 final class Day6 extends BaseAdventOfCodeDay
 {
     public function one(): void
     {
+        $fishCount = $this->getFishAfterDays(80);
+
+        $this->info(
+            'Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?',
+        );
+        $this->line($fishCount);
+    }
+
+    public function two(): void
+    {
+        $fishCount = $this->getFishAfterDays2(256);
+
+        $this->info('How many lanternfish would there be after 256 days?');
+        $this->line($fishCount);
+    }
+
+    protected function getInputNumbers(): array
+    {
+        return array_map(
+            static fn($number) => (int) $number,
+            explode(',', $this->input[0]),
+        );
+    }
+
+    protected function getFishAfterDays(int $days): int
+    {
         $fish = $this->getInputNumbers();
         $day = 0;
-        while ($day < 80) {
+        while ($day < $days) {
             $fishToAdd = 0;
             foreach ($fish as $index => $number) {
                 if (--$number < 0) {
@@ -32,22 +61,54 @@ final class Day6 extends BaseAdventOfCodeDay
             $day++;
         }
 
-        $this->info(
-            'Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?',
-        );
-        $this->line(count($fish));
+        return count($fish);
     }
 
-    public function two(): void
+    protected function getFishAfterDays2(int $days): int
     {
-        // TODO: Implement two() method.
-    }
+        $fish = $this->getInputNumbers();
+        $matrix = array_reduce(
+            $fish,
+            static function ($carry, $current) {
+                if (!array_key_exists($current, $carry)) {
+                    $carry[(int) $current] = 1;
+                } else {
+                    $carry[(int) $current]++;
+                }
 
-    protected function getInputNumbers(): array
-    {
-        return array_map(
-            static fn($number) => (int) $number,
-            explode(',', $this->input[0]),
+                return $carry;
+            },
+            [],
         );
+
+        ksort($matrix);
+
+        $day = 0;
+        while ($day < $days) {
+            $dMatrix = [];
+            foreach ($matrix as $dDay => $fishCount) {
+                $dMatrix[$dDay - 1] = $fishCount;
+            }
+
+            if (array_key_exists(-1, $dMatrix)) {
+                $fishToReproduce = $dMatrix[-1];
+                unset($dMatrix[-1]);
+
+                if (array_key_exists(6, $dMatrix)) {
+                    $dMatrix[6] += $fishToReproduce;
+                } else {
+                    $dMatrix[6] = $fishToReproduce;
+                }
+
+                $dMatrix[8] = $fishToReproduce;
+            }
+
+            $matrix = $dMatrix;
+            ksort($matrix);
+
+            $day++;
+        }
+
+        return array_sum($matrix);
     }
 }

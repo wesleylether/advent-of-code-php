6011269d214cd8af3e4a301193d3e2a87e45b98d
Wesley Lether <wesleylether@gmail.com>
2021-12-07 13:12:15 +0100

    Added Year 2021 Day 7 part 2

--- app/AdventOfCode/year2021/Day7.php
+++ app/AdventOfCode/year2021/Day7.php
@@ -28,7 +28,33 @@ final class Day7 extends BaseAdventOfCodeDay
 
     public function two(): void
     {
-        // TODO: Implement two() method.
+        $crabs = $this->getPuzzleInput();
+        sort($crabs);
+
+        $best = 1e11;
+        $min = min($crabs);
+        $max = max($crabs);
+        foreach (range($min, $max) as $pos) {
+            $fuel = 0;
+            foreach ($crabs as $crab) {
+                $d = abs($crab - $pos);
+                $fuel += $this->calculateFuelUse($d);
+            }
+
+            if ($fuel < $best) {
+                $best = $fuel;
+            }
+        }
+
+        $this->info(
+            'Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?',
+        );
+        $this->line($best);
+    }
+
+    protected function calculateFuelUse(int $delta): int
+    {
+        return ($delta * ($delta + 1)) / 2;
     }
 
     protected function getPuzzleInput(): array

89e232519faf932e793e93a4db11d34dc2a97938
Wesley Lether <wesleylether@gmail.com>
2021-12-10 13:33:39 +0100

    Refactor 2021 / 10

--- app/AdventOfCode/year2021/Day10.php
+++ app/AdventOfCode/year2021/Day10.php
@@ -9,6 +9,7 @@ use function array_pop;
 use function array_reduce;
 use function array_reverse;
 use function count;
+use function key;
 use function last;
 use function str_split;
 
@@ -24,107 +25,26 @@ final class Day10 extends BaseAdventOfCodeDay
         ];
 
         $count = 0;
-        $corrupted = 0;
-
-        foreach ($this->input as $line) {
-            $data = [];
-            foreach (str_split($line) as $i => $char) {
-                switch ($char) {
-                    case '(':
-                    case '{':
-                    case '[':
-                    case '<':
-                        $data[] = $char;
-                        continue 2;
-                    case ')':
-                        if (last($data) === '(') {
-                            array_pop($data);
-                            continue 2;
-                        }
-                        break;
-                    case '}':
-                        if (last($data) === '{') {
-                            array_pop($data);
-                            continue 2;
-                        }
-                        break;
-                    case ']':
-                        if (last($data) === '[') {
-                            array_pop($data);
-                            continue 2;
-                        }
-                        break;
-                    case '>':
-                        if (last($data) === '<') {
-                            array_pop($data);
-                            continue 2;
-                        }
-                        break;
-                }
-
-                $count += $lookup[$char];
-                $corrupted++;
-                continue 2;
-            }
+        [$corrupted] = $this->getData();
+        foreach ($corrupted as $data) {
+            $count += $lookup[key($data)];
         }
 
         $this->info(
             'Find the first illegal character in each corrupted line of the navigation subsystem. What is the total syntax error score for those errors?',
         );
-        $this->warn("Corrupted: $corrupted");
         $this->line($count);
     }
 
     public function two(): void
     {
-        $incomplete = [];
-        foreach ($this->input as $line) {
-            $data = [];
-            foreach (str_split($line) as $i => $char) {
-                switch ($char) {
-                    case '(':
-                    case '{':
-                    case '[':
-                    case '<':
-                        $data[] = $char;
-                        continue 2;
-                    case ')':
-                        if (last($data) === '(') {
-                            array_pop($data);
-                            continue 2;
-                        }
-                        break;
-                    case '}':
-                        if (last($data) === '{') {
-                            array_pop($data);
-                            continue 2;
-                        }
-                        break;
-                    case ']':
-                        if (last($data) === '[') {
-                            array_pop($data);
-                            continue 2;
-                        }
-                        break;
-                    case '>':
-                        if (last($data) === '<') {
-                            array_pop($data);
-                            continue 2;
-                        }
-                        break;
-                }
-                continue 2;
-            }
-
-            $incomplete[] = $data;
-        }
-
         $lookup = [
             ')' => 1,
             ']' => 2,
             '}' => 3,
             '>' => 4,
         ];
+        [, $incomplete] = $this->getData();
 
         $scores = [];
         foreach ($incomplete as $line) {
@@ -149,5 +69,55 @@ final class Day10 extends BaseAdventOfCodeDay
             'Find the completion string for each incomplete line, score the completion strings, and sort the scores. What is the middle score?',
         );
         $this->line($scores[floor(count($scores) * 0.5)]);
+    }
+
+    protected function getData(): array
+    {
+        $corrupted = [];
+        $incomplete = [];
+        foreach ($this->input as $line) {
+            $data = [];
+            foreach (str_split($line) as $char) {
+                switch ($char) {
+                    case '(':
+                    case '{':
+                    case '[':
+                    case '<':
+                        $data[] = $char;
+                        continue 2;
+                    case ')':
+                        if (last($data) === '(') {
+                            array_pop($data);
+                            continue 2;
+                        }
+                        break;
+                    case '}':
+                        if (last($data) === '{') {
+                            array_pop($data);
+                            continue 2;
+                        }
+                        break;
+                    case ']':
+                        if (last($data) === '[') {
+                            array_pop($data);
+                            continue 2;
+                        }
+                        break;
+                    case '>':
+                        if (last($data) === '<') {
+                            array_pop($data);
+                            continue 2;
+                        }
+                        break;
+                }
+
+                $corrupted[] = [$char => $line];
+                continue 2;
+            }
+
+            $incomplete[] = $data;
+        }
+
+        return [$corrupted, $incomplete];
     }
 }
